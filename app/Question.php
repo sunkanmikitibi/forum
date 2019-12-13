23f65fdb6dda0900d2a05297de1252efa8a6c31d
sunkanmi <sunkanmikitibi@gmail.com>
2019-12-13 10:21:07 +0100

    Add author info and date of question creation in all questions

--- app/Question.php
+++ app/Question.php
@@ -10,7 +10,7 @@ class Question extends Model
 
     public function user()
     {
-        return $this->belongsTo('App\Question');
+        return $this->belongsTo('App\User');
     }
 
     public function setTitleAttribute($value)
@@ -18,7 +18,16 @@ class Question extends Model
         $this->attributes['title'] = $value;
 
         $this->attributes['slug'] = str_slug($value);
-         }
+     }
+
+     public function getUrlAttribute()
+     {
+         return route("questions.show", $this->id);
+     }
+
+     public function getCreatedDateAttribute(){
+         return $this->created_at->diffForHumans();
+     }
 
 
 }
--- app/User.php
+++ app/User.php
@@ -41,4 +41,10 @@ class User extends Authenticatable
     {
         return $this->hasMany('App\Question');
     }
+
+    public function getUrlAttribute()
+    {
+       // return route("question.show", $this->id);
+       return "#";
+    }
 }
